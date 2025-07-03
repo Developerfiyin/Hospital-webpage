67eca25b90be4a71af6c1a6c59d582fd9b9e011a
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-07-03 22:43:14 +0100

    added stop animation on resize in animation

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $("feedback-slider").owlCarousel({
+  $(".feedback-slider").owlCarousel({
     loop: false,
     margin: 10,
     nav: true,
@@ -8,5 +8,15 @@ $(document).ready(function () {
     navText: [
       "<i class = 'fas fa-arrow-left'></i>, <i class = 'fas fa-arrow-right'></i>",
     ],
+  });
+
+  /*STOP ANIMATION ON RESIZE */
+  let resignation;
+  $(window).resize(function () {
+    $(document.body).addClass("resize-animation-stopper");
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+      $(document.body).removeClass("resize-animation-stopper");
+    }, 400);
   });
 });
